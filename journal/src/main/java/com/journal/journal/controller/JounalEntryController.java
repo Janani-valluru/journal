c10bb4d61f66e9beededb1f8f091bb983849c9a1
Janani-valluru <janani.valluru@gmail.com>
2024-10-18 00:04:52 +0530

    rest api

--- journal/src/main/java/com/journal/journal/controller/JounalEntryController.java
+++ journal/src/main/java/com/journal/journal/controller/JounalEntryController.java
@@ -5,8 +5,11 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,5 +36,26 @@ public class JounalEntryController {
         // and it return boolean value
         journalEntries.put(myEntry.getId(), myEntry);
         return true;
+    }
+
+    /**
+     * Retrieve a journal entry by its id
+     * 
+     * @param id the id of the journal entry
+     * @return the journal entry with the given id
+     */
+    @GetMapping("id/{myId}")
+    public JounalEntry getEntry(@PathVariable long myId) {
+        return journalEntries.get(myId);
+    }
+
+    @DeleteMapping("id/{myId}")
+    public JounalEntry deletJounalEntryById(@PathVariable long myId) {
+        return journalEntries.remove(myId);
+    }
+
+    @PutMapping
+    public JounalEntry updateJounalEntry(@PathVariable Long id, @RequestBody JounalEntry myEntry) {
+        return journalEntries.put(id, myEntry);
     }
 }
